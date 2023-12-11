4636afff9520b579020adf0134cf3f6bf6147268
Julian_Eder <julian.eder@laabmayr.at>
2023-12-11 18:55:10 +0100

    initial svelte project setup

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+  import MainPage from "./lib/MainPage.svelte";
+  import UploadImage from "./lib/UploadImage.svelte";
+</script>
+
+<main>
+  <MainPage>
+    <div class="parent-container grid">
+      <div class="col1">
+        <p>Upload Image</p>
+        <UploadImage></UploadImage>
+      </div>
+      <div class="col2">
+        <svg
+          width="500"
+          height="300"
+          viewBox="0 0 500 300"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="0"
+            y="0"
+            width="500"
+            height="300"
+            stroke="black"
+            stroke-width="3"
+            fill="red"
+          />
+          <path 
+            d="M 10 10 q 200 200 300 0"
+            stroke="blue"
+            stroke-width="1" fill="none"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  </MainPage>
+</main>
+
+<style>
+
+  .col1{
+    padding: 20px;
+  }
+  .col2{
+    padding: 20px;
+  }
+  .parent-container {
+    border: 2px black solid;
+    /* width: 100%; */
+  }
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+</style>
---
+++ src/lib/UploadImage.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    let inputImage: any, fileinput: any;
+
+    const onFileSelected = (e: Event) => {
+        let image = e.target?.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = (e) => {
+            inputImage = e.target?.result;
+        };
+    };
+</script>
+
+<div id="app">
+    {#if inputImage}
+        <img 
+            class="inputImage" 
+            src={inputImage} 
+            alt="d" 
+        />
+    {:else}
+        <div class="placehodlerImage">
+
+        </div>
+    {/if}
+
+    <button
+        class="upload"
+        on:click={() => {
+            fileinput.click();
+        }}
+    >Upload Image</button>
+    <input
+        style="display:none"
+        type="file"
+        accept=".jpg, .jpeg, .png"
+        on:change={(e) => onFileSelected(e)}
+        bind:this={fileinput}
+    />
+</div>
+
+<style>
+    #app {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-flow: column;
+    }
+    .upload {
+        height: 50px;
+        width: 150px;
+        cursor: pointer;
+        background-color: #9af4fa;
+        margin: 20px;
+    }
+    .inputImage {
+        display: flex;
+        max-width: 50%;
+        max-height: 100%;
+    }
+    .placehodlerImage{
+        border-color: #000;
+        background-color: #f8f8f8;
+        border-style:solid;
+        border-width: 1px;
+        border-radius: 10px;
+        width: 100px;
+        height: 100px;
+    }
+</style>
