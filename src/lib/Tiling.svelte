3fd8724ebf0626d2287818688fddf0b9396156e6
Julian_Eder <julian.eder@laabmayr.at>
2024-01-11 14:43:51 +0100

    readme, restructuring, basic skelletonization

---
+++ src/lib/Tiling.svelte
@@ -0,0 +1,158 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { BBox, type Edge, SitePoint, SiteSegment, type Vertex } from "./voronoiDataStructures";
+  import instantiate_wasmVoronoi, { type VoronoiWasmModule, type EdgeResult } from "../lib/wasm/wasmVoronoi"
+
+  let wasmVoronoi: VoronoiWasmModule;
+
+
+  let bbox: BBox = { xl: 0, xh: 500, yl: 0, yh: 300 };
+
+  let sitePoints: SitePoint[] = [];
+  let siteSegments: SiteSegment[] = [];
+  let voronoiVertices: Vertex[] = [];
+  let voronoiEdges: Edge[] = [];
+
+  function wasmWraper(){
+    
+    const bboxVector = new wasmVoronoi.VectorDouble();
+    bboxVector.push_back(bbox.xl)
+    bboxVector.push_back(bbox.yl)
+    bboxVector.push_back(bbox.xh)
+    bboxVector.push_back(bbox.yh)
+
+    const pointVector = new wasmVoronoi.VectorInt();
+    sitePoints.forEach(sp => {
+      pointVector.push_back(sp.x)
+      pointVector.push_back(sp.y)
+    });
+    const segmentVector = new wasmVoronoi.VectorInt();
+    siteSegments.forEach(ss => {
+      segmentVector.push_back(ss.x1)
+      segmentVector.push_back(ss.y1)
+      segmentVector.push_back(ss.x2)
+      segmentVector.push_back(ss.y2)
+    });
+    
+    let result = wasmVoronoi.computevoronoi(bboxVector, pointVector, segmentVector);
+    let newVoronoiVertices: Vertex[] = [];
+    for (var i = 0; i < result.vertices.size(); i+=2) {
+      // console.log("Vert: ", result.vertices.get(i));
+      newVoronoiVertices.push({x: result.vertices.get(i), y: result.vertices.get(i+1)})
+    }
+    
+    let newVoronoiEdges: Edge[] = [];
+    for (var i = 0; i < result.edges.size(); i++) {
+      let e: EdgeResult = result.edges.get(i);
+      let samples: Vertex[] = [];
+      for (var j = 0; j < e.samples.size(); j+=2) {
+        samples.push({x: e.samples.get(j), y: e.samples.get(j+1)})
+      }
+      let controlPoints: Vertex[] = [];
+      for (var j = 0; j < e.controll_points.size(); j+=2) {
+        controlPoints.push({x: e.controll_points.get(j), y: e.controll_points.get(j+1)})
+      }
+
+      newVoronoiEdges.push({
+        va: {x: e.x1, y: e.y1},
+        vb: {x: e.x2, y: e.y2},
+        samples: samples,
+        controlPoints: controlPoints,
+        isPrimary: e.isPrimary
+      })
+    }
+
+    // console.log(newVoronoiEdges)
+    voronoiVertices = newVoronoiVertices;
+    voronoiEdges = newVoronoiEdges;
+  }
+
+  onMount(async () => {
+    
+    wasmVoronoi = await instantiate_wasmVoronoi();
+
+    sitePoints = [new SitePoint(200, 200), new SitePoint(300, 100), new SitePoint(100, 100)];
+    siteSegments = [new SiteSegment(10, 200, 400, 250), new SiteSegment(380, 50, 400, 200)];
+
+    wasmWraper();
+
+
+  });
+
+  function addPoint(event: MouseEvent) {
+    sitePoints = [...sitePoints, new SitePoint(event.offsetX, event.offsetY)]; // This syntax triggeres Sveltes reactive reload
+    wasmWraper();
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <svg
+          width="500"
+          height="300"
+          viewBox="{bbox.xl} {bbox.yl} {bbox.xh} {bbox.yh}"
+          xmlns="http://www.w3.org/2000/svg"
+          on:click={addPoint}
+        >
+          <rect
+            x="0"
+            y="0"
+            width="500"
+            height="300"
+            stroke="black"
+            stroke-width="3"
+            fill="rgb(240, 238, 231)"
+          />
+          {#each sitePoints as siteP, idx}
+            <circle cx={siteP.x} cy={siteP.y} r="4" fill="black"></circle>
+          {/each}
+          {#each siteSegments as siteS, idx}
+            <path
+              d="M {siteS.x1} {siteS.y1} L {siteS.x2} {siteS.y2}"
+              stroke="black"
+              stroke-width="2"
+              fill="none"
+            ></path>
+            <circle cx={siteS.x1} cy={siteS.y1} r="4" fill="black"></circle>
+            <circle cx={siteS.x2} cy={siteS.y2} r="4" fill="black"></circle>
+          {/each}
+          <!-- {#each voronoiVertices as v, idx}
+            <circle cx={v.x} cy={v.y} r="5" fill="red"></circle>
+          {/each} -->
+          {#each voronoiEdges as e, idx}
+            <circle cx={e.va.x} cy={e.va.y} r="3" fill="green"></circle>
+            <circle cx={e.vb.x} cy={e.vb.y} r="3" fill="green"></circle>
+            {#if e.samples.length == 0}
+              {#if e.isPrimary}
+                <path
+                d="M {e.va.x} {e.va.y} L {e.vb.x} {e.vb.y}"
+                stroke="blue"
+                stroke-width="1"
+                fill="none"
+                ></path>
+              {:else}
+                <path
+                d="M {e.va.x} {e.va.y} L {e.vb.x} {e.vb.y}"
+                stroke="green"
+                stroke-width="1"
+                fill="none"
+                ></path>
+              {/if}
+            {:else}
+            <path d="M {e.va.x} {e.va.y} {e.samples.map((s) => 'L ' + s.x + ' ' + s.y).reduce((a,b) => a + ' ' + b)} "
+              stroke="blue"
+              stroke-width="1"
+              fill="none"
+              ></path>
+              <path d="M {e.controlPoints[0].x} {e.controlPoints[0].y} Q {e.controlPoints[1].x} {e.controlPoints[1].y} {e.controlPoints[2].x} {e.controlPoints[2].y}"
+                stroke="red"
+                stroke-width="1"
+                fill="none"
+                ></path>
+              {/if}
+          {/each}
+        </svg>
+
+<style>
+    
+</style>
--- src/lib/UploadImage.svelte
+++ src/lib/UploadImage.svelte
@@ -1,20 +1,96 @@
 <script lang="ts">
+    
+    import Range from "./Range.svelte"	
+    import TraceSkeleton from './trace_skeleton.vanilla';
+    import TraceSkeleton2  from './wasmSkelleton/index'
+    import { onMount } from "svelte";
+
+    let tracer: any;
+
     let inputImage: any, fileinput: any;
 
-    const onFileSelected = (e: Event) => {
+    let canvas: any;
+    let ctx: any;
+    let svgContainer: any;
+
+    let width = 100;
+    let height = 100;
+
+    let resolution: number = 25;
+
+    let svgStr: string = "";
+
+    const onFileSelected = (e: any) => {
+        Promise.resolve().then(() => handleFileUpload(e)); // Run Async
+    };
+    const handleFileUpload = (e: any) => {
         let image = e.target?.files[0];
         let reader = new FileReader();
         reader.readAsDataURL(image);
         reader.onload = (e) => {
             inputImage = e.target?.result;
+            
+            let image = new Image();
+            image.onload = () => Promise.resolve().then(() => {
+                width = image.width;
+                height = image.height;
+                ctx = canvas.getContext('2d', {
+                    willReadFrequently: true,
+                });                
+                ctx.fillStyle = ctx.createPattern(image, 'repeat'); 
+                ctx.fillRect(0, 0, width, height);
+
+                updateSkelleton();
+
+   	        });
+		    image.src = inputImage;
+            // let img = inputImage;
+
         };
+    }
+
+    const onResolutionChanged = (newRes: number) => {
+        resolution = newRes;
+        Promise.resolve().then(updateSkelleton); // Run Async
+    
     };
+    const updateSkelleton = () => {
+        
+        let imageData = ctx.getImageData(0,0,width,height);
+        // let { polylines,rects } = tracer.fromImageData(imageData, resolution); // resolution is ignored in WASM for now
+        let { polylines,rects } = TraceSkeleton.fromImageData(imageData, resolution);
+        svgStr = TraceSkeleton.visualize({ polylines,rects, width, height }, {rects: true})
+        svgContainer.innerHTML = svgStr;
+    };
+
+    onMount(async () => {
+        tracer = await TraceSkeleton2.load();
+    });
+
 </script>
 
 <div id="app">
-    {#if inputImage}
+    <div class="drawingContainer">
+
+        <canvas class="uploadCanvas"
+            bind:this={canvas}
+            width=377 
+            height=309
+            style="width: 377px; height: 309px"
+        ></canvas>
+        <div
+            class="overdrawSvg"
+            bind:this={svgContainer}
+        ></div>
+    </div>
+    <div class:purple-theme={false} class="resolution-slider">
+        <label for="basic-range">Resolution</label>
+        <Range min={5} max={100} initialValue={resolution} on:change={(e) => onResolutionChanged(e.detail.value)} />
+    </div>
+    <!-- {#if inputImage}
         <img 
             class="inputImage" 
+            bind:this={htmlImg}
             src={inputImage} 
             alt="d" 
         />
@@ -22,7 +98,7 @@
         <div class="placehodlerImage">
 
         </div>
-    {/if}
+    {/if} -->
 
     <button
         class="upload"
@@ -66,5 +142,26 @@
         border-radius: 10px;
         width: 100px;
         height: 100px;
+    }
+    .drawingContainer{
+        position: relative;
+
+    }
+    .uploadCanvas{
+        border-color: #000;
+        background-color: #f8f8f8;
+        border-style:solid;
+        border-width: 1px;
+    }
+    .overdrawSvg{
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 377px; 
+        height: 309px;
+        z-index: 10;
+    }
+    .resolution-slider{
+        width: 300px;
     }
 </style>
